162bb4c15e57018b91cf765252dc854be421dfd5
Shaik, Baji Rasheed Ali <bajirasheedali.s@infosys.com>
2024-08-14 16:15:48 +0530

    add: error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import createHttpError, { HttpError } from 'http-errors'
 
 const app = express()
+
+app.get('/', (req, res) => {
+    const err = createHttpError.Unauthorized()
+    throw err
+    res.json({ message: 'Hello World' })
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500
+
+    res.status(statusCode).json({
+        message: err.message,
+        errStack: process.env.NODE_ENV === 'production' ? '🤫' : err.stack,
+    })
+})
 
 export default app
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,7 @@ dotenvConfig()
 const _config = {
     port: process.env.PORT,
     dbString: process.env.CONNECTION_STRING,
+    env: process.env.NODE_ENV,
 }
 
 export const config = Object.freeze(_config)
